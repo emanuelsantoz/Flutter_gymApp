b08f5d7527b4ab006ef6b975060b68abe4d5c7bb
emanuelsantoz <emanuelsantossouzajesus@gmail.com>
2024-05-14 11:43:30 -0300

    Criando components

--- gyamapp/lib/autenticacao_tela.dart
+++ gyamapp/lib/autenticacao_tela.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:gyamapp/_comum/minhas_cores.dart';
+import 'package:gyamapp/components/decoracao_campo_auteticacao.dart';
+
+class AutenticacaoTela extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AutenticacaoStats();
+  }
+}
 
 // ignore: must_be_immutable
-class AutenticacaoTela extends StatelessWidget {
+class AutenticacaoStats extends State {
   bool queroEntrar = true;
-
-  AutenticacaoTela({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -27,40 +33,68 @@ class AutenticacaoTela extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Form(
-                child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Image.asset("assets/logo.png", height: 158),
-                  const Text(
-                    textAlign: TextAlign.center,
-                    "GymApp",
-                    style: TextStyle(
-                        fontSize: 48,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
-                  ),
-                  SizedBox(height: 32),
-                  TextFormField(decoration: InputDecoration(label: Text("E-mail")),),
-                  TextFormField(decoration: InputDecoration(label: Text("Senha")), obscureText: true, ),
-                  SizedBox(height: 16),
-                  ElevatedButton(onPressed: () {},
-                  child: Text((queroEntrar) ? "Entrar" : "Cadastrar")),
-                  Divider(),
-                  TextButton(
-                    onPressed: () {
-                    setState(( ) {
-                      queroEntrar = !queroEntrar;
-                      print(queroEntrar);
-                    });
-                  }, child: Text("Ainda não tenho uma conta"))
-                ],
-              ),
+                child: Center(
+                  child: SingleChildScrollView(
+                      child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Image.asset("assets/logo.png", height: 158),
+                      const Text(
+                        textAlign: TextAlign.center,
+                        "GymApp",
+                        style: TextStyle(
+                            fontSize: 48,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      SizedBox(height: 32),
+                      TextFormField(
+                        decoration: getAutenticationInputDecoraton("E-mail")
+                      ),
+                      SizedBox(height: 8),
+                      TextFormField(
+                        decoration: getAutenticationInputDecoraton("Senha"),
+                        obscureText: true,
+                      ),
+                      SizedBox(height: 8),
+                      Visibility(
+                          visible: !queroEntrar,
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                decoration: getAutenticationInputDecoraton("Confirme senha"),
+                                obscureText: true,
+                              ),
+                              SizedBox(
+                                height: 8
+                              ),
+                              TextFormField(
+                                decoration: getAutenticationInputDecoraton("Nome"),
+                              ),
+                            ],
+                          )),
+                      SizedBox(height: 16),
+                      ElevatedButton(
+                          onPressed: () {},
+                          child: Text((queroEntrar) ? "Entrar" : "Cadastrar")),
+                      Divider(),
+                      TextButton(
+                          onPressed: () {
+                            setState(() {
+                              queroEntrar = !queroEntrar;
+                              print(queroEntrar);
+                            });
+                          },
+                          child: Text((queroEntrar)
+                              ? "Ainda não tenho uma conta? Cadastre-se"
+                              : "Já tem conta? Entre!")),
+                    ],
+                  )),
+                ),
               ),
             )
-            
           ],
         ));
   }
-  
 }
---
+++ gyamapp/lib/components/decoracao_campo_auteticacao.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:gyamapp/_comum/minhas_cores.dart';
+
+InputDecoration getAutenticationInputDecoraton(String label){
+  return InputDecoration(
+    hintText: label,
+    fillColor: const Color.fromARGB(255, 255, 255, 255),
+    filled: true,
+    contentPadding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+    border: OutlineInputBorder(borderRadius: BorderRadius.circular(32)),
+    enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(32), borderSide: BorderSide(color: Colors.black, width: 2)),
+    focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(32), borderSide: BorderSide(color: MinhasCores.azulTopoGradiente, width: 4))
+  );
+}
